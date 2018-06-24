cc7b131b200c287cc4e6b0d6cb6d98f13b51105a
Renisha Christie. A <christie.reni@gmail.com>
2018-06-25 00:30:03 +0530

    Add or remove classes depending on window size

--- src/app/body.js
+++ src/app/body.js
@@ -1,9 +1,24 @@
 // Calculate body area
-let header = document.querySelector('header');
-let footer = document.querySelector('footer');
-let headerHeight = header.offsetHeight;
-let footerHeight = footer.offsetHeight;
-let windowHeight = window.innerHeight;
-let main = document.querySelector('#main');
-let mainHeight = windowHeight - (headerHeight + footerHeight);
+const header = document.querySelector('header');
+const footer = document.querySelector('footer');
+const headerHeight = header.offsetHeight;
+const footerHeight = footer.offsetHeight;
+const windowHeight = window.innerHeight;
+const main = document.querySelector('#main');
+const mainHeight = windowHeight - (headerHeight + footerHeight);
 main.style.height = mainHeight + 'px';
+
+// Add or remove classes depending on window size
+const winWidth = window.innerWidth;
+const field = document.querySelector('.field');
+const vertical = document.querySelector('.is-vertical-center');
+const control = document.querySelectorAll('.control');
+if (winWidth < 640) {
+    field.classList.add('is-grouped', 'is-grouped-multiline');
+    field.classList.remove('has-addons', 'has-addons-centered');
+    vertical.classList.remove('is-vertical-center');
+    for (var each of control) {
+        each.classList.add('is-expanded');
+        each.firstElementChild.classList.add('is-fullwidth');
+    }
+}
